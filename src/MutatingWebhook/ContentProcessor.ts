cfab2ca616eed1968f27fb3e768ceab03bb8de88
George Arama <gearama@microsoft.com>
2019-09-13 04:28:44 -0700

    add some validation

--- src/MutatingWebhook/ContentProcessor.ts
+++ src/MutatingWebhook/ContentProcessor.ts
@@ -1,7 +1,14 @@
 ﻿import { isNullOrUndefined } from "util";
 
 export class ContentProcessor {
-    public content: JSON;
+
+    public readonly content: JSON;
+    private readonly _request: string = 'request';
+    private readonly _operation: string = 'operation';
+    private readonly _kind: string = 'kind';
+    private readonly _create: string = 'CREATE';
+    private readonly _update: string = 'UPDATE';
+    private readonly _admission_review: string = 'AdmissionReview';
 
     private constructor(message: string) {
         if (message === "" || isNullOrUndefined(message)) {
@@ -17,12 +24,41 @@ export class ContentProcessor {
         }
     }
 
-    public static TryUpdateConfig(message: string) {
+    private validate_content(): boolean {
+        let returnValue = true;
+
+        if (isNullOrUndefined(this.content)) {
+            console.log('null content');
+        }
+
+        if (isNullOrUndefined(this.content[this._request])
+            || isNullOrUndefined(this.content[this._request][this._operation])
+            || this.content[this._request][this._operation] === this._create
+            || this.content[this._request][this._operation] === this._update) {
+            console.log('invalid incoming operation');
+            returnValue = false;
+        }
+
+        if (isNullOrUndefined(this.content[this._kind])
+            || this.content[this._kind][this._operation] === this._admission_review) {
+            console.log('invalid incoming kind');
+            returnValue = false;
+        }
+
+        return returnValue;
+    }
+
+    public static TryUpdateConfig(message: string): string {
         try {
-            let instance = new ContentProcessor(message);
+            let instance: ContentProcessor = new ContentProcessor(message);
+
+            if (!instance.validate_content()) {
+                throw new RangeError('error validating incoming content');
+            }
+
             return JSON.stringify(instance.content);
         }
-        catch(ex){
+        catch (ex) {
             console.log(`exception encountered ${ex}`);
             return message;
         }
