8a6f458e714516fdbb401234775fcdc27b19874f
George Arama <gearama@microsoft.com>
2019-11-11 14:18:30 -0800

    done

--- agents/build.cmd
+++ agents/build.cmd
@@ -1,6 +1,6 @@
 setlocal
 call docker build -t mutating-webhook-agents . --no-cache
-call az acr login --name gearamaaks
-call docker tag mutating-webhook-agents gearamaaks.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook-agents:%1
-call docker push gearamaaks.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook-agents:%1
+call az acr login --name applicationinsights
+call docker tag mutating-webhook-agents applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook-agents:%1
+call docker push applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook-agents:%1
 endlocal
--- src/dockerBuild.cmd
+++ src/dockerBuild.cmd
@@ -1,6 +1,6 @@
 setlocal
 call docker build -t mutating-webhook . --no-cache
-call az acr login --name gearamaaks
-call docker tag mutating-webhook gearamaaks.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:%1
-call docker push gearamaaks.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:%1
+call az acr login --name applicationinsights
+call docker tag mutating-webhook applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:%1
+call docker push applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:%1
 endlocal
