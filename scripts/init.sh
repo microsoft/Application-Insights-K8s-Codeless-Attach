13656283508d55ca91d46316b571f02f0d3580ef
George Arama <gearama@microsoft.com>
2019-11-12 12:23:39 -0800

    path

--- scripts/init.sh
+++ scripts/init.sh
@@ -111,4 +111,4 @@ kubectl create secret generic ${title} \
     kubectl -n ${namespace} apply -f -
 
 export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
-cat ./values._aml | envsubst > ../helm_package/values.yaml
+cat ./values._aml | envsubst > ./values.yaml
