cb1aa5857f214bc485f6b340d83bdff67b709aed
George Arama <gearama@microsoft.com>
2019-12-11 12:54:09 -0800

    updates to agents

--- agents/build.cmd
+++ agents/build.cmd
@@ -1,7 +1,7 @@
 setlocal
 REM we need download here
 REM call tar -xf .\agents.zip -C .\agents
-call docker build -t mutating-webhook-agents . --no-cache --build-arg AGENT_VERSION=2.8.33
+call docker build -t mutating-webhook-agents . --no-cache --build-arg AGENT_VERSION=%2
 call az acr login --name applicationinsights
 call docker tag mutating-webhook-agents applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook-agents:%1
 call docker push applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook-agents:%1
