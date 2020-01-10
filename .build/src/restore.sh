1d297d1f7900623756016f6a6eaf6d97d619458f
George Arama <gearama@microsoft.com>
2020-01-09 17:56:07 -0800

    start

--- .build/src/restore.sh
+++ .build/src/restore.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y -q update
+
 # install docker 
 apt-get -y -q remove docker docker-engine docker.io
 apt-get -y -q install docker.io
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # install node 10.0
 apt-get install curl 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get -y -q install nodejs
 
+# run npm install
 cd `dirname $0`
 SCRIPTDIR=`pwd`
 cd ${SCRIPTDIR}/../../src
