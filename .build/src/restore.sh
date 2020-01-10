35754314dcfc73772a2ee665c748dd77a5f85b05
George Arama <gearama@microsoft.com>
2020-01-09 17:22:34 -0800

    node.js

--- .build/src/restore.sh
+++ .build/src/restore.sh
@@ -2,6 +2,9 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y -q update
 apt-get -y -q install docker
+apt-get install curl 
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt install nodejs
 cd `dirname $0`
 SCRIPTDIR=`pwd`
 cd ${SCRIPTDIR}/../../src
