fd2bde7363ba6b9015496a6611085c216591ef9e
George Arama <gearama@microsoft.com>
2020-01-22 16:20:26 -0800

    acr test

--- .build/src/acrpush.sh
+++ .build/src/acrpush.sh
@@ -3,4 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 cd `dirname $0`
 SCRIPTDIR=`pwd`
 cd ${SCRIPTDIR}/../../src
-docker push applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:latest
+/usr/bin/docker build -t mutating-webhook . --no-cache
+az acr login --name applicationinsights
+/usr/bin/docker tag mutating-webhook applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:goqu
+/usr/bin/docker push applicationinsights.azurecr.io/public/applicationinsights/codeless-attach/mutating-webhook:goqu
