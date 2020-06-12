8b5410aa5d4abb75ac59ee36f5179d82c0a70474
George Arama <gearama@microsoft.com>
2020-06-12 16:24:13 -0700

    fix sizes

--- samples/LoadGenerator/Function1.cs
+++ samples/LoadGenerator/Function1.cs
@@ -16,7 +16,7 @@ namespace LoadGenerator
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             String jsonString = "{\"DelayMs\":\"100\",\"RetryCount\":\"1\",\"FailureChance\":\"0.4\",\"SubsequentCalls\":[{\"Uri\": \"http://correlation.southcentralus.cloudapp.azure.com:8086/api/query/GetString?fromAks=true&delayInMS=100&backendNumRetries=1&dataStoreDelayInMS=1&dataStoreFailureRatio=0.1&dataStoreTextFailureRatio=0.05\",\"Params\":\"RedisParams BlobParams\"}]}";
             HttpClient httpClient = new HttpClient();
-            var result = await httpClient.PostAsync("http://13.66.156.119/", new StringContent(jsonString, Encoding.UTF8, "application/json"));
+            var result = await httpClient.PostAsync("http://52.143.75.114/", new StringContent(jsonString, Encoding.UTF8, "application/json"));
             Console.WriteLine(result.ToString());
             return ;
         }
