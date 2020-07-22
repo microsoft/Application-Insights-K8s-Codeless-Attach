71b609ab6251feeb346db0dc9e69e612f56ecf94
George Arama <gearama@microsoft.com>
2020-07-22 01:24:43 -0700

    node agent and tailing

--- agents/logsuploader/main.go
+++ agents/logsuploader/main.go
@@ -1,27 +1,101 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"time"
 )
 
+const windowsFolder = "./samples"
+const linuxFolder = "/var/log/applicationinsights"
+
 func main() {
-	fmt.Println("Hello, logs uploader.")
-	var name string
-	client := appinsights.NewTelemetryClient("320dcf98-173f-429b-ab39-df8b4951fb94")
-	metric := appinsights.NewMetricTelemetry("Queue length", 1)
-	event := appinsights.NewEventTelemetry("event")
-	metric.Properties["Queue name"] = "queuename"
-	client.Track(metric)
-	client.Track(event)
-	fmt.Println("Done starting")
-	fmt.Scan(&name)
-	/*t, err := tail.TailFile("/var/log/nginx.log", tail.Config{Follow: true})
-	if err != nil {
-		panic(err)
+	fmt.Println("Starting logs uploader.")
+
+	folder := linuxFolder
+	if runtime.GOOS == "windows" {
+		folder = windowsFolder
 	}
-	for line := range t.Lines {
-		fmt.Println(line.Text)
-	}*/
+
+	availableFiles, err := pickupFiles(folder, false) // attempt to pick up files
+
+	if err != nil {
+		fmt.Println("No files available to upload at startup")
+	} else {
+		tailFiles(availableFiles)
+	}
+
+	fmt.Println("Done starting")
+
+	availableFiles, _ = pickupFiles(folder, true)
+
+	tailFiles(availableFiles)
+
+	/*
+		client := appinsights.NewTelemetryClient("320dcf98-173f-429b-ab39-df8b4951fb94")
+		metric := appinsights.NewMetricTelemetry("Queue length", 1)
+		event := appinsights.NewEventTelemetry("event")
+		metric.Properties["Queue name"] = "queuename"
+		client.Track(metric)
+		client.Track(event)
+		fmt.Println("Done starting")
+
+		t, err := tail.TailFile("/var/log/nginx.log", tail.Config{Follow: true})
+		if err != nil {
+			panic(err)
+		}
+		for line := range t.Lines {
+			fmt.Println(line.Text)
+		}
+	*/
+}
+
+func tailFiles(files []os.FileInfo) {
+	for _, file := range files {
+		// here we be tailing the file
+		fmt.Println("tailing file", file.Name())
+	}
+}
+
+func pickupFiles(folder string, retry bool) ([]os.FileInfo, error) {
+
+	var availableFiles []os.FileInfo
+	var err error
+
+	for {
+		availableFiles, err = getFiles(folder)
+		if err == nil {
+			for index, file := range availableFiles {
+				fmt.Println(index, "=>", file.Name())
+			}
+			return availableFiles, nil
+		} else if retry {
+			fmt.Println("waiting for files to become available")
+			time.Sleep(1 * time.Second)
+		} else {
+			fmt.Println("attempted to pickup files, nothing to report, not waiting")
+			return nil, errors.New("No files detected")
+		}
+	}
+}
+
+func getFiles(folder string) ([]os.FileInfo, error) {
+	if folder == "" {
+		fmt.Println("error finding data")
+		return nil, errors.New("folder cannot be null")
+	}
+
+	files, err := ioutil.ReadDir(folder)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+	return files, nil
 }
--- agents/node/ai-telemetry.js
+++ agents/node/ai-telemetry.js
@@ -1,4 +1,37 @@
-﻿const path = require('path');
+﻿const { start } = require('repl');
 
-console.log("hello world from second agent!");
+const linuxPath = "/agentfiles/telemetry/logsuploader/logsuploader";
+const windowsPath = "D:\\src\\Application-Insights-K8s-Codeless-Attach\\agents\\logsuploader\\logsuploader.exe";
+let path = process.platform === "linux" ? linuxPath : windowsPath;
+
+console.log("Logs uploader starting");
+
+async function starter() {
+    await new Promise((resolve, reject) => {
+        child = require('child_process').execFile(path, [], {
+            // detachment and ignored stdin are the key here: 
+            detached: true,
+            stdio: ['ignore', 1, 2]
+        }, err => {
+            console.log(`failed to start process ${path}, \n ${err}`);
+            resolve();
+        });
+
+        // and unref() disentangles the child's event loop from the parent's: 
+        child.unref();
+
+        child.stdout.on('data', function (data) {
+            let entry = data.toString();
+            console.log(entry);
+            if (entry.indexOf("Done starting") > -1) {
+                resolve();
+            }
+        });
+    })
+}
+
+starter();
+
+console.log("Initialized logs uploader");
+
 
